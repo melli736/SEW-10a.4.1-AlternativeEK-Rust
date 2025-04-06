f0558ad858552cbad3166754578de6be83d35ee0
Melissa Wallpach <mwallpach@student.tgm.ac.amwallpach@student.tgm.ac.at>
2025-04-06 18:31:25 +0200

    created rust project

---
+++ rustWallpachEK/target/debug/deps/rustWallpachEK-dd00a90a0c5f27ce.d
@@ -0,0 +1,5 @@
+/Users/melli/Library/CloudStorage/OneDrive-tgm-DieSchulederTechnik/5BHIT/SEW/SEW-10a.4.1-AlternativeEK-Rust/rustWallpachEK/target/debug/deps/librustWallpachEK-dd00a90a0c5f27ce.rmeta: src/main.rs
+
+/Users/melli/Library/CloudStorage/OneDrive-tgm-DieSchulederTechnik/5BHIT/SEW/SEW-10a.4.1-AlternativeEK-Rust/rustWallpachEK/target/debug/deps/rustWallpachEK-dd00a90a0c5f27ce.d: src/main.rs
+
+src/main.rs:
---
+++ rustWallpachEK/target/debug/deps/rustWallpachEK-e1aa3bb2c3958fe3.d
@@ -0,0 +1,5 @@
+/Users/melli/Library/CloudStorage/OneDrive-tgm-DieSchulederTechnik/5BHIT/SEW/SEW-10a.4.1-AlternativeEK-Rust/rustWallpachEK/target/debug/deps/librustWallpachEK-e1aa3bb2c3958fe3.rmeta: src/main.rs
+
+/Users/melli/Library/CloudStorage/OneDrive-tgm-DieSchulederTechnik/5BHIT/SEW/SEW-10a.4.1-AlternativeEK-Rust/rustWallpachEK/target/debug/deps/rustWallpachEK-e1aa3bb2c3958fe3.d: src/main.rs
+
+src/main.rs:
